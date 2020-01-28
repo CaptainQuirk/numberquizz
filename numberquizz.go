d74ed75a208e0f34c6fecf621374fe4617389009
lmessier <leonard.messier@gmail.com>
2020-01-28 23:01:48 +0100

    feat: prompt the user

--- numberquizz.go
+++ numberquizz.go
@@ -3,10 +3,15 @@ package main
 import (
     "fmt"
     "github.com/CaptainQuirk/numberquizz/generator"
+    "bufio"
+    "os"
 )
 
 func main() {
     question := fmt.Sprintf("Turn %d into %s: ", generator.GenerateInt(255), generator.GenerateTarget())
 
-    fmt.Println(question)
+    reader := bufio.NewReader(os.Stdin)
+    fmt.Print(question)
+    text, _ := reader.ReadString('\n')
+    fmt.Println(text)
 }
