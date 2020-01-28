9d908f6b06be556864c62772298b7b9035eb3879
lmessier <leonard.messier@gmail.com>
2020-01-29 00:36:12 +0100

    feat: use the solver to solve the problem

--- numberquizz.go
+++ numberquizz.go
@@ -3,15 +3,23 @@ package main
 import (
     "fmt"
     "github.com/CaptainQuirk/numberquizz/generator"
+    "github.com/CaptainQuirk/numberquizz/solver"
     "bufio"
     "os"
+    "strings"
 )
 
 func main() {
     statement := generator.GenerateStatement()
+    solution := solver.Solve(statement)
 
     reader := bufio.NewReader(os.Stdin)
     fmt.Print(statement.ToQuestion())
     text, _ := reader.ReadString('\n')
-    fmt.Println(text)
+
+    if (strings.TrimRight(answer, "\n") != solution) {
+        fmt.Printf("Error ! Answer was « %s » and got « %s »\n", solution, answer)
+    } else {
+        fmt.Println("Congrats ! You win !")
+    }
 }
