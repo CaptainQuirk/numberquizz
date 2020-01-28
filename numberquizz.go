57705416e06e037dba621ad175c94f4b8e8f062e
lmessier <leonard.messier@gmail.com>
2020-01-29 00:50:30 +0100

    refactor: use ToLower only to compare to preserve user input

--- numberquizz.go
+++ numberquizz.go
@@ -11,7 +11,6 @@ import (
 
 func filter(text string) string {
     text = strings.TrimRight(text, "\n")
-    text = strings.ToLower(text)
 
     return text
 }
@@ -28,7 +27,7 @@ func main() {
     text, _ := reader.ReadString('\n')
     answer := filter(text)
 
-    if (answer != solution) {
+    if (strings.ToLower(answer) != solution) {
         fmt.Printf("Error ! Answer was « %s » and got « %s »\n", solution, answer)
     } else {
         fmt.Println("Congrats ! You win !")
